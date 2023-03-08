5f00e690f48c6ff7deea181e5a96d71bd1bedd6d
An Nguyen <annguyen@Ans-MacBook-Pro.lan>
2023-03-08 11:25:43 -0600

    update

--- server/db/Products.js
+++ server/db/Products.js
@@ -1,19 +1,19 @@
 const client = require('./client');
 
 async function createProduct({name, price, description, shipping, weight, picture}){
-    try{
-        const product = `
-        INSERT INTO products(name, price, description, shipping, weight, picture)
-        VALUES($1,$2,$3,$4,$5,$6)
-        RETURNING *
-        `;
-        const response = await client.query(product, [name, price, description, shipping, weight, picture]);
-        return response.rows[0];
-    }
-    catch(error){
-        console.error("Error creating products")
-        throw error;
-    }
+    // try{
+    //     const product = `
+    //     INSERT INTO products(name, price, description, shipping, weight, picture)
+    //     VALUES($1,$2,$3,$4,$5,$6)
+    //     RETURNING *
+    //     `;
+    //     const response = await client.query(product, [name, price, description, shipping, weight, picture]);
+    //     return response.rows[0];
+    // }
+    // catch(error){
+    //     console.error("Error creating products")
+    //     throw error;
+    // }
 }
 
 async function getProductById(id){
