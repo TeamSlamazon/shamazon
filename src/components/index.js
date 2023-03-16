28102498e8458a7dea3bec7af5e4c33e225f0c84
erictorres0123 <erictorres0123@gmail.com>
2023-03-16 00:22:09 -0500

    export

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ export { default as Header} from './Header'
 export { default as Footer} from './Footer'
 export { default as Products} from './Products'
 export { default as Register} from './Register'
-
+export { default as Logout } from './Logout'
+export { default as Checkout } from './Checkout'
